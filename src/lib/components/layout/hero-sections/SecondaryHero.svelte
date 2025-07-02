643aff0a2c5cff169148bc91d467245917b855ae
Uncle Common <krishnerkar@gmail.com>
2025-07-02 14:59:25 +0000

    Update hero copy with concise text

--- src/lib/components/layout/hero-sections/SecondaryHero.svelte
+++ src/lib/components/layout/hero-sections/SecondaryHero.svelte
@@ -16,12 +16,14 @@
 <header class="border-b border-gray-100 dark:border-gray-900" {...rest}>
 	<div class="text-title3 section-px section-py container mx-auto grid gap-16 gap-y-0">
 		<SectionHeader {title} {subtitle} size="lg" />
-		<div class="aspect-video overflow-clip rounded-xl bg-gray-100">
-			<img
-				class="h-full w-full object-cover"
-				src={imageSrc}
-				alt="Diverse group of people collaborating"
-			/>
-		</div>
+		{#if imageSrc}
+			<div class="aspect-video overflow-clip rounded-xl bg-gray-100">
+				<img
+					class="h-full w-full object-cover"
+					src={imageSrc}
+					alt="Diverse group of people collaborating"
+				/>
+			</div>
+		{/if}
 	</div>
 </header>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -79,9 +79,7 @@
 	<SecondaryHero
 		title="Building the future of organizational intelligence"
 		subtitle="Intelligent alignment for growing teams."
-		imageSrc="https://images.unsplash.com/photo-1750658659691-1f7b2bd37337?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 	/>
-
 	<!-- Team Section -->
 	<CompactTeam
 		title="Who we are"
