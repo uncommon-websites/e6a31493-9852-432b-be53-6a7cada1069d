8624f3e3356bb08f43c6a3eb1783ad8297ab388f
Uncle Common <krishnerkar@gmail.com>
2025-07-02 11:06:52 +0000

    chore: preserve manual changes before workflow

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -52,12 +52,12 @@ export const cta = {
 };
 
 export const navigation: NavItem[] = [
-	{
-		label: "Pricing",
-		href: "/pricing",
-		showInNav: true,
-		showInFooter: false
-	},
+	// {
+	// 	label: "Pricing",
+	// 	href: "/pricing",
+	// 	showInNav: true,
+	// 	showInFooter: false
+	// },
 	{
 		label: "Company",
 		showInNav: true,
